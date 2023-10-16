417ec437bc4fe3983668b996379c7ebda9748850
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-16 15:16:58 -0400

    added a loop to run through all outfits

--- src/OutfitsIndex.jsx
+++ src/OutfitsIndex.jsx
@@ -1,7 +1,15 @@
-export function OutfitsIndex() {
+export function OutfitsIndex(props) {
   return (
     <div>
       <h1>All outfits</h1>
+      {props.outfits.map((outfit) => (
+        <div key={outfit.id}>
+          <h2>{outfit.day}</h2>
+          {/* <img src={outfit.url} /> */}
+          <p>Top: {outfit.top_id}</p>
+          <p>Bottom: {outfit.bottom_id}</p>
+        </div>
+      ))}
     </div>
   );
 }
