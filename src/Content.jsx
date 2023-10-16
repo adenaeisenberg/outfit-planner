823cf75739a1c83aa74bfa09d1c132e96ac2520b
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-16 15:14:15 -0400

    added placeholder data

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,15 @@
 import { OutfitsIndex } from "./OutfitsIndex";
 
 export function Content() {
+  const outfits = [
+    { id: 1, day: "Sunday", top_id: 1, bottom_id: 1 },
+    { id: 2, day: "Monday", top_id: 1, bottom_id: 2 },
+  ];
+
   return (
     <div>
       <h1>Outfit Planner App</h1>
-      <OutfitsIndex />
+      <OutfitsIndex outfits={outfits} />
     </div>
   );
 }
