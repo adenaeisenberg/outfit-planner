9b9c3da7eb84c1480d96a1a2da7502f2f007aaca
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-18 15:35:15 -0400

    ability to pass props

--- src/Content.jsx
+++ src/Content.jsx
@@ -39,7 +39,7 @@ export function Content() {
       <LogoutLink />
       <OutfitsNew onCreateOutfit={handleCreateOutfit} />
       <OutfitsIndex outfits={outfits} />
-      <TopsIndex />
+      <TopsIndex tops={tops} />
     </div>
   );
 }
