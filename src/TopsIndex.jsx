ec416700c3577f0d267ebb2814cf12b8e416a3bc
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-18 15:39:31 -0400

    added a loop that will run through all the tops and index them

--- src/TopsIndex.jsx
+++ src/TopsIndex.jsx
@@ -1,7 +1,14 @@
-export function TopsIndex() {
+/* eslint-disable react/prop-types */
+export function TopsIndex(props) {
   return (
     <div>
       <h1>All Tops</h1>
+      {props.tops.map((top) => (
+        <div key={top.id}>
+          <h2>{top.name}</h2>
+          <img src={top.image_url} />
+        </div>
+      ))}
     </div>
   );
 }
