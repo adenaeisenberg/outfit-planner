35ae52fcbd74ade898b3c9f8dbbb4f396bbd345f
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-18 15:33:27 -0400

    imported TopsIndex to content page

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,8 +5,10 @@ import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
 import { OutfitsNew } from "./OutfitsNew";
+import { TopsIndex } from "./TopsIndex";
 
 export function Content() {
+  // Outfit section //
   const [outfits, setOutfits] = useState([]);
 
   const handleIndexOutfits = () => {
@@ -27,6 +29,8 @@ export function Content() {
 
   useEffect(handleIndexOutfits, []);
 
+  // Tops section
+
   return (
     <div>
       <h1>Outfit Planner App</h1>
@@ -35,6 +39,7 @@ export function Content() {
       <LogoutLink />
       <OutfitsNew onCreateOutfit={handleCreateOutfit} />
       <OutfitsIndex outfits={outfits} />
+      <TopsIndex />
     </div>
   );
 }
