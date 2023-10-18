19fdcdaf4229fdc6d3a05683780d216bf743c043
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-18 16:11:40 -0400

    this function maps through all bottoms

--- src/BottomsIndex.jsx
+++ src/BottomsIndex.jsx
@@ -2,6 +2,12 @@ export function BottomsIndex(props) {
   return (
     <div>
       <h1>All Bottoms</h1>
+      {props.bottoms.map((bottom) => (
+        <div key={bottom.id}>
+          <h2>{bottom.name}</h2>
+          <img src={bottom.image_url} width={300} />
+        </div>
+      ))}
     </div>
   );
 }
