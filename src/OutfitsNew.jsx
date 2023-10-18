351df753154b2ed460788f1a0b04cc62f4b0ce5d
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-18 15:22:55 -0400

    adds a HandleSubmit to run the props

--- src/OutfitsNew.jsx
+++ src/OutfitsNew.jsx
@@ -1,8 +1,14 @@
-export function OutfitsNew() {
+export function OutfitsNew(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onCreateOutfit(params, () => event.target.reset());
+  };
+
   return (
     <div>
       <h1>New Outfit</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           Day: <input name="day" type="text" />
         </div>
