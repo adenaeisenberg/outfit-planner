8899f94bcbfeb9621340a6164351dd2b93f06c44
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-18 15:56:53 -0400

    handle submit event to run the given props

--- src/TopsNew.jsx
+++ src/TopsNew.jsx
@@ -1,8 +1,15 @@
-export function TopsNew() {
+/* eslint-disable react/prop-types */
+export function TopsNew(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onCreateTop(params, () => event.target.reset());
+  };
+
   return (
     <div>
       <h1>Add Top</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
         </div>
