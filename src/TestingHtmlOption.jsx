2ccc768f75c4e952c8402130be7ccce4eba935d7
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-22 11:58:51 -0400

    i made a testing page for my dropdown using html features

--- src/TestingHtmlOption.jsx
+++ src/TestingHtmlOption.jsx
@@ -1,39 +1,56 @@
+import "./Dropdown.css";
+
 export function TestingHtmlOption() {
   return (
-    <><head>
-      <title>Adding image to dropdown list</title>
- 
-    </head><body>
+    <>
+      <head>
+        <title>Adding image to dropdown list</title>
+      </head>
+      <body>
         <center>
-          <h1 style="color: green">
-            GeeksforGeeks
-          </h1>
+          <h1 style="color: green">GeeksforGeeks</h1>
 
-          <div class="dropdown">
-            <button class="dropbtn">
-              Country Flags
-            </button>
+          <div className="dropdown">
+            <button className="dropbtn">Country Flags</button>
 
-            <div class="dropdown-content">
+            <div className="dropdown-content">
               <a href="#">
-                <img src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132503/iflag.jpg"
-                  width="20" height="15"> India</></a>
+                <img
+                  src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132503/iflag.jpg"
+                  width="20"
+                  height="15"
+                />{" "}
+                India{" "}
+              </a>
 
               <a href="#">
-                <img src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132504/uflag.jpg"
-                  width="20" height="15"> USA</></a>
+                <img
+                  src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132504/uflag.jpg"
+                  width="20"
+                  height="15"
+                />{" "}
+                USA
+              </a>
               <a href="#">
-                <img src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132502/eflag.jpg"
-                  width="20" height="15"> England</></a>
+                <img
+                  src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132502/eflag.jpg"
+                  width="20"
+                  height="15"
+                />{" "}
+                England
+              </a>
               <a href="#">
-                <img src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132500/bflag.jpg"
-                  width="20" height="15"> Brazil</></a>
+                <img
+                  src="https://media.geeksforgeeks.org/wp-content/uploads/20200630132500/bflag.jpg"
+                  width="20"
+                  height="15"
+                />{" "}
+                Brazil
+              </a>
             </div>
           </div>
         </center>
       </body>
-      </> 
-
-  )
-  
+    </>
+  );
 }
