0e2d0333764b30ff6c55a5cfb0ee60b030db26c6
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-22 10:51:47 -0400

    changed title on top left corner

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ export function Header() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            Outfit Planner App
           </a>
           <button
             className="navbar-toggler"
