f2e0f40adbbc88a382cab6621b6c3954045f48ee
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-26 16:56:09 -0400

    put bottoms in cards

--- src/BottomsIndex.jsx
+++ src/BottomsIndex.jsx
@@ -14,12 +14,22 @@ export function BottomsIndex(props) {
   return (
     <div>
       <h1>All Bottoms</h1>
-      {props.bottoms.map((bottom) => (
-        <div key={bottom.id}>
-          <h2>{bottom.name}</h2>
-          <img src={bottom.image_url} width={300} />
-        </div>
-      ))}
+      <div className="row">
+        {props.bottoms.map((bottom) => (
+          <div key={bottom.id} className="card" style={{ width: "18rem" }}>
+            <img className="card-img-bottom" src={bottom.image_url} alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">{bottom.name}</h5>
+              {/* <p className="card-text"> Insert text here</p> */}
+              <a href="/outfits" className="btn btn-primary">
+                Wear this bottom!
+              </a>
+            </div>
+          </div>
+        ))}
+        <br />
+      </div>
+      <br />
 
       <BottomsNew onCreateBottom={handleCreateBottom} />
     </div>
