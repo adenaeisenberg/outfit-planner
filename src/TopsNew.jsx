13114c97ae4b2cca77c36b2e105feaae586ef2c2
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-26 16:53:01 -0400

    deleted commented out code

--- src/TopsNew.jsx
+++ src/TopsNew.jsx
@@ -24,25 +24,4 @@ export function TopsNew(props) {
       </div>
     </div>
   );
-  // }
-
-  // <div className="card w-100">
-  //     <h5 className="card-title">Card title</h5>
-  //     <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-  //   </div>
-  // </div>;
-
-  // {
-  /* <div>
-<h1>Add Top</h1>
-<form onSubmit={handleSubmit}>
-  <div>
-    Name: <input name="name" type="text" />
-  </div>
-  <div>
-    Image Url: <input name="image_url" type="text" />
-  </div>
-  <button type="submit">Add Top</button>
-</form>
-</div> */
 }
