14b7acdb9909512ffa116ff030fbbbdbef0086b7
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-26 16:52:03 -0400

    put Add Top in a card

--- src/TopsNew.jsx
+++ src/TopsNew.jsx
@@ -7,17 +7,42 @@ export function TopsNew(props) {
   };
 
   return (
-    <div>
-      <h1>Add Top</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input name="name" type="text" />
-        </div>
-        <div>
-          Image Url: <input name="image_url" type="text" />
-        </div>
-        <button type="submit">Add Top</button>
-      </form>
+    <div className="card w-100">
+      <div className="card-body">
+        <h3 className="card-title">Add Top</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            Name: <input name="name" type="text" />
+          </div>
+          <div>
+            Image Url: <input name="image_url" type="text" />
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Add Top
+          </button>
+        </form>
+      </div>
     </div>
   );
+  // }
+
+  // <div className="card w-100">
+  //     <h5 className="card-title">Card title</h5>
+  //     <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  //   </div>
+  // </div>;
+
+  // {
+  /* <div>
+<h1>Add Top</h1>
+<form onSubmit={handleSubmit}>
+  <div>
+    Name: <input name="name" type="text" />
+  </div>
+  <div>
+    Image Url: <input name="image_url" type="text" />
+  </div>
+  <button type="submit">Add Top</button>
+</form>
+</div> */
 }
