48f4fbf2e5e64e881faa43a846a9b5755ac5f773
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-26 17:20:25 -0400

    centered text in card:

--- src/BottomsNew.jsx
+++ src/BottomsNew.jsx
@@ -8,19 +8,21 @@ export function BottomsNew(props) {
 
   return (
     <div className="card w-100">
-      <div className="card-body">
-        <h3 className="card-title">Add Bottom</h3>
-        <form onSubmit={handleSubmit}>
-          <div>
-            Name: <input name="name" type="text" />
-          </div>
-          <div>
-            Image Url: <input name="image_url" type="text" />
-          </div>
-          <button className="btn btn-primary" type="submit">
-            Add Bottom
-          </button>
-        </form>
+      <div className="card text-center">
+        <div className="card-body">
+          <h3 className="card-title">Add Bottom</h3>
+          <form onSubmit={handleSubmit}>
+            <div>
+              Name: <input name="name" type="text" />
+            </div>
+            <div>
+              Image Url: <input name="image_url" type="text" />
+            </div>
+            <button className="btn btn-primary" type="submit">
+              Add Bottom
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
