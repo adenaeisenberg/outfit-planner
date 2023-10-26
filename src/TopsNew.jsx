2c3936d4cc25492b7704aa072718448f7731c547
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-10-26 17:22:52 -0400

    centered text in card:

--- src/TopsNew.jsx
+++ src/TopsNew.jsx
@@ -8,19 +8,21 @@ export function TopsNew(props) {
 
   return (
     <div className="card w-100">
-      <div className="card-body">
-        <h3 className="card-title">Add Top</h3>
-        <form onSubmit={handleSubmit}>
-          <div>
-            Name: <input name="name" type="text" />
-          </div>
-          <div>
-            Image Url: <input name="image_url" type="text" />
-          </div>
-          <button className="btn btn-primary" type="submit">
-            Add Top
-          </button>
-        </form>
+      <div className="card text-center">
+        <div className="card-body">
+          <h3 className="card-title">Add Top</h3>
+          <form onSubmit={handleSubmit}>
+            <div>
+              Name: <input name="name" type="text" />
+            </div>
+            <div>
+              Image Url: <input name="image_url" type="text" />
+            </div>
+            <button className="btn btn-primary" type="submit">
+              Add Top
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
