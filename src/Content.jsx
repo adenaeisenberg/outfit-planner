6bf8e5b2abef15944734f8e6c1ed1faf4c44f617
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-11-02 17:07:34 -0400

    adds 'content' to calendar 30 days before and 30 days aftercurrent date

--- src/Content.jsx
+++ src/Content.jsx
@@ -9,7 +9,7 @@ import { BottomsIndex } from "./BottomsIndex";
 
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import { compareAsc, format } from "date-fns";
+import { addDays, eachDayOfInterval, isSameDay, subDays } from "date-fns";
 
 export function Content() {
   // Outfit section //
@@ -56,17 +56,34 @@ export function Content() {
 
   // https://www.youtube.com/watch?v=WutUO81wE90
   const [date, setDate] = useState(new Date());
-  const datesToAddContentTo = [new Date()];
+
+  const today = new Date();
+  const numberOfDays = 30; // Adjust this number based on how many days you want
+
+  const pastDates = Array.from({ length: numberOfDays }, (_, index) => subDays(today, index + 1));
+  const futureDates = Array.from({ length: numberOfDays }, (_, index) => addDays(today, index));
+
+  const allDates = [...pastDates, today, ...futureDates];
 
   function tileContent({ date, view }) {
     // Add class to tiles in month view only
     if (view === "month") {
       // Check if a date React-Calendar wants to check is on the list of dates to add class to
-      if (datesToAddContentTo.find((dDate) => date.getDay() === dDate.getDate())) {
+      if (allDates.find((d) => isSameDay(d, date))) {
         return "My content";
       }
     }
   }
+
+  // function tileContent({ date, view }) {
+  //   // Add class to tiles in month view only
+  //   if (view === "month") {
+  //     // Check if a date React-Calendar wants to check is on the list of dates to add class to
+  //     if (datesToAddContentTo.find((dDate) => date.getDay() === dDate.getDate())) {
+  //       return "My content";
+  //     }
+  //   }
+  // }
 
   // tileContent={({ outfit, date, view }) => (view === "month" && date.getDay() === 0 ? <p>It's Sunday!</p> : null)} // this is where I need to put the logic about pulling the specific day's outfit
 
