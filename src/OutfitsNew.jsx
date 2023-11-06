177b669de0f665ee56628a41f1dd7959f7e97873
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-11-06 16:30:58 -0500

    moves the content of the card into the correct padding

--- src/OutfitsNew.jsx
+++ src/OutfitsNew.jsx
@@ -13,44 +13,45 @@ export function OutfitsNew(props) {
   return (
     <div className="card w-100">
       <div className="card text-center">
-        <div className="card-body"></div>
-        <h1 className="card-title">New Outfit</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            Day: <input name="day" type="text" />
-          </div>
+        <div className="card-body">
+          <h1 className="card-title">New Outfit</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              Day: <input name="day" type="text" />
+            </div>
 
-          <div className="dropdown">
-            Top:
-            <select className="dropbtn" name="top_id" id="tops">
-              {props.tops.map((top) => (
-                <option
-                  value={top.id}
-                  key={top.id}
-                  onMouseEnter={console.log("works")}
-                  onMouseLeave={() => this.removeTopImage}
-                >
-                  {top.name}
-                </option>
-              ))}
-            </select>
-          </div>
+            <div className="dropdown">
+              Top:
+              <select className="dropbtn" name="top_id" id="tops">
+                {props.tops.map((top) => (
+                  <option
+                    value={top.id}
+                    key={top.id}
+                    onMouseEnter={console.log("works")}
+                    onMouseLeave={() => this.removeTopImage}
+                  >
+                    {top.name}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-          <div className="dropdown">
-            Bottom:
-            <select className="dropbtn" name="bottom_id" id="bottoms">
-              {props.bottoms.map((bottom) => (
-                <option value={bottom.id} key={bottom.id}>
-                  {bottom.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <br />
-          <button className="btn btn-primary" type="submit">
-            Add Outfit to Wardrobe
-          </button>
-        </form>
+            <div className="dropdown">
+              Bottom:
+              <select className="dropbtn" name="bottom_id" id="bottoms">
+                {props.bottoms.map((bottom) => (
+                  <option value={bottom.id} key={bottom.id}>
+                    {bottom.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <br />
+            <button className="btn btn-primary" type="submit">
+              Add Outfit to Wardrobe
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
