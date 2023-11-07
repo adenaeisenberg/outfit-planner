689c96f3216e8f402f1cf6be59be519155d4e831
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-11-07 17:37:02 -0500

    changed styling for button TopsNew

--- src/TopsNew.jsx
+++ src/TopsNew.jsx
@@ -1,4 +1,7 @@
 /* eslint-disable react/prop-types */
+
+import "./Button_89.css";
+
 export function TopsNew(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -18,7 +21,7 @@ export function TopsNew(props) {
             <div>
               Image Url: <input name="image_url" type="text" />
             </div>
-            <button className="btn btn-primary" type="submit">
+            <button className="button-89" type="submit">
               Add Top
             </button>
           </form>
